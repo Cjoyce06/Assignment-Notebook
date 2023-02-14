83aad3a6d242b9e6d30d7b8a2b8554b82285e012
Colin Joyce <Cjoyce1@bsd220.org>
2023-02-14 17:01:56 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Colin Joyce on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
